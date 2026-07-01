b6976cc37555c6cc601cb44a2d92c4bf9d0cd602
Angel Awasthi <angelawasthi76@gmail.com>
2026-07-01 22:44:06 +0530

    Time: 14 ms (28.26%), Space: 46.7 MB (88.3%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        
+        int i=0;
+
+        while(i<arr.length){
+            
+            if(arr[i]==0){
+                for(int j=arr.length-1;j>i;j--){
+                    arr[j]=arr[j-1];
+                }
+                i++;
+            }
+            i++;
+        }
+    }
+}
