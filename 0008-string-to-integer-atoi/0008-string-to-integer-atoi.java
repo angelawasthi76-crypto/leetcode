2d125c6e793f3f181e0973131538bd2cb7a1744e
Angel Awasthi <angelawasthi76@gmail.com>
2026-07-05 22:23:32 +0530

    Time: 1 ms (100%), Space: 44.1 MB (12.48%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -3,44 +3,45 @@ class Solution {
     public static boolean isDigit(char c){
         return c>='0' && c<='9';
     }
+    public int helper(String s,long num,int i,int sign){
 
-    public int myAtoi(String s) {
-        
+        if(i>=s.length() || !isDigit(s.charAt(i))){
+            return (int)num*sign;
+        }
+        int digit = s.charAt(i) - '0';
+
+        if (num > Integer.MAX_VALUE / 10 ||
+   (num == Integer.MAX_VALUE / 10 &&
+   ((sign == 1 && digit > 7) ||
+    (sign == -1 && digit > 8)))) {
+
+    return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+}
+
+num = num * 10 + digit;
+        return helper(s,num,i+1,sign);
+    }
+
+    public int func(String s){
         int i=0;
-        int num=0;
-
-        int sign =1;
+        long num=0;
+        int sign=1;
 
         while(i<s.length() && s.charAt(i)==' '){
             i++;
         }
-
-        if(i<s.length()){
-            if(s.charAt(i)=='-'){
-                sign =-1;
-                i++;
-            }
-            else if( s.charAt(i)=='+'){
-                i++;
-            }
+        if(i<s.length() && s.charAt(i)=='-'){
+            sign=-1;
+            i++;
         }
-
-        while(i<s.length() && isDigit(s.charAt(i))){
-            int digit = s.charAt(i) - '0';
-
-            if(num == Integer.MAX_VALUE /10){
-                if(sign ==1 && digit >=7)return Integer.MAX_VALUE;
-                else if(sign ==-1 && digit>=8)return Integer.MIN_VALUE;
-            } 
-            if(num>Integer.MAX_VALUE /10){
-                if(sign ==1)return Integer.MAX_VALUE;
-                else return Integer.MIN_VALUE;
-            }
-
-            num=num*10+digit;
+        else if(i<s.length() && s.charAt(i)=='+'){
             i++;
         }
 
-        return sign *num;
+        return helper(s,num,i,sign);
+    }
+
+    public int myAtoi(String s) {
+        return (int)func(s);
     }
 }
