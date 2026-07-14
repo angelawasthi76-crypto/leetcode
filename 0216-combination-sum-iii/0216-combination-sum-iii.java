6ac9a451d5bd9d607b56b217e3f74bc14b7f00a6
Angel Awasthi <angelawasthi76@gmail.com>
2026-07-14 11:07:54 +0530

    Time: 0 ms (100%), Space: 42.4 MB (66.71%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -2,24 +2,24 @@ class Solution {
 
     List<List<Integer>> ans = new ArrayList<>();
 
-     private void backtrack( int target, int index,int k, List<Integer> curr,int cnt) {
-        if (target == 0 && cnt ==k) {
+     private void backtrack( int target, int index,int k, List<Integer> curr) {
+        if (target == 0 && curr.size() ==k) {
             ans.add(new ArrayList<>(curr));
             return;
         }
 
-        if (cnt>k || target < 0) {
+        if (curr.size()>k || target < 0) {
             return;
         }
 
         for (int i = index; i <=9; i++) {
             curr.add(i);
-            backtrack( target - i, i+1,k, curr,cnt+1); // reuse same element
+            backtrack( target - i, i+1,k, curr); // reuse same element
             curr.remove(curr.size() - 1); // backtrack
         }
     }
     public List<List<Integer>> combinationSum3(int k, int n) {
-        backtrack(n,1,k,new ArrayList<>(),0);
+        backtrack(n,1,k,new ArrayList<>());
         return ans;
     }
 }
